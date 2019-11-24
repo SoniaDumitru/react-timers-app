6f211497dfe3380779a65f9b45836659464f5c5c
Sonia Dumitru <soni.dumitru@gmail.com>
2019-11-24 15:38:46 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Timer from './Timer'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+constructor() {
+  super()
+    this.state = {
+      timerIDS: []
+    }
 }
+
+  handleAddTimer =()=> {
+    let timerIDS = this.state.timerIDS
+    this.setState({
+      timerIDS: [...timerIDS, Math.floor(Math.random()*1000)]
+    })
+    // console.log(this.state.timerIDS)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Timer</h1>
+          <button onClick={this.handleAddTimer}>Add New Timer</button>
+          <div>{this.state.timerIDS.map(id => <Timer id={id} key={id}/>)}</div>
+      </div>
+    );
+  }
+}
+
 
 export default App;
