ebe67e0fd9a69d3b163532e547c757d5498719f2
SoniaDumitru <soni.dumitru@gmail.com>
2019-11-24 17:10:09 -0600

    Update Timer.js

--- src/Timer.js
+++ src/Timer.js
@@ -1,13 +1,43 @@
 import React from 'react'
 
 class Timer extends React.Component {
+  constructor(props) {
+    super(props)
+      this.state = {
+        time: 0
+      }
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(this.clockTick, 1000)
+  }
+
+  clockTick = () => {
+    this.setState({
+      time: this.state.time + 1
+    })
+  }
+
+  handleClose = () => {
+    this.props.removeTimer(this.props.id)
+  }
+
+  stopClock = () => {
+    clearInterval(this.interval)
+  }
+
   render() {
     return(
-      <>
-        <div>I am the timer</div>
-      </>
+
+        <section>
+          <h1> {this.state.time} seconds elapsed since mounting! </h1>
+          <button onClick= {this.handleClose}>x</button>
+          <button onClick={ this.stopClock }>Stop</button>
+        </section>
+
     )
   }
 }
 
 export default Timer
+
